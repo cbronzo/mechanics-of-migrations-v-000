ce25a311084dd563609a1b136867696eb6ea211a
Catherine Bronzo <cbronzo@mac.com>
2018-07-28 00:52:09 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,16 +1,14 @@
 class CreateArtists < ActiveRecord::Migration
-  
   def up
   end
- 
-  def down
+   def down
   end
-  
-  def change
+   def change
     create_table :artists do |t|
       t.string :name
       t.string :genre
       t.integer :age
       t.string :hometown
+    end
   end
-end
+ end
