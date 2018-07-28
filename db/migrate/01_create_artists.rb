7dcbe49f7ce300acd85edf4b348cd63f41a24074
Catherine Bronzo <cbronzo@mac.com>
2018-07-28 00:39:16 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -8,3 +8,8 @@ class CreateArtists < ActiveRecord::Migration
   def change
   end
 end
+
+connection = ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
